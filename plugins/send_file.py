0137932310d61bddd08baff69366406358b78a0d
Akvcontact2 <anujkumarverma177@gmail.com>
2024-07-19 10:03:49 +0530

    Update send_file.py

--- plugins/send_file.py
+++ plugins/send_file.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from plugins.helpers import str_to_b64
+from database import db
 
 
 async def reply_forward(message: Message, file_id: int):
@@ -33,6 +34,10 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
+    try:
+        await db.delete_file_id(user_id)
+    except:        
+        pass
     asyncio.create_task(delete_after_delay(sent_message, 1800))
 
 async def delete_after_delay(message, delay):
